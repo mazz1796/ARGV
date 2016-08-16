63388e96a7652a342ef8bc72717f6195f17c2f99
Rudzainy_Rahman <rudzainy@gmail.com>
2016-08-16 01:47:54 -0400

    Update argv_basics.rb

--- argv_basics.rb
+++ argv_basics.rb
@@ -1,1 +1,23 @@
-# Place your solutions here
+# Place your pig latin solution here
+
+
+
+# Place your RPN calculator solution here
+class RPNCalculator
+
+  def evaluate(string)
+  end
+
+  def calculation(string)
+  end
+end
+
+calc = RPNCalculator.new
+
+puts calc.evaluate('1 2 +')   # => 3
+puts calc.evaluate('2 5 *')   # => 10
+puts calc.evaluate('50 20 -') # => 30
+
+# The general rule is that 'A B op' is the same as 'A op B'
+# i.e., 5 4 - is 5 - 4.
+puts calc.evaluate('70 10 4 + 5 * -') # => 0
